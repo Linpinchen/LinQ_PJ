925186ea68101409d075eee84cf993b7c4f1bdc0
林品辰 <s10325347@gmail.com>
2024-01-08 00:42:10 +0800

    LinQ 說明以及練習

---
+++ LinqPJ/Assets/NewBehaviourScript.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using UnityEngine.Analytics;
+using System.Security.Cryptography.X509Certificates;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public List<Number> numbers;
+    public List<Data> datas;
+
+    void Start()
+    {
+        // 透過 LinQ 尋找 “指定值” 於一維集合中 的索引值
+        IEnumerable<int> ints = numbers.Select((a, i) => i).Where(i => numbers[i] == (int)Number.one);
+
+        foreach (var i in ints)
+        {
+            Debug.Log(i);
+        }
+
+        //------------------------------------------------------------------------------------------------------------
+        //< 1. >算指定值出現的總數 （一維）
+
+
+        int oneCount = numbers.Count(i => i == Number.one);
+        Debug.Log($"OneCount : {oneCount}");
+
+
+        //-----------------------------------------------------------------------------------------------------------------------
+        // < 2. > 算指定值出現的總數 (二維)(整個盤面的)
+
+
+        var twoCount = datas.Sum(row => row.Reels.Count(Item => Item == (int)Number.one));
+        Debug.Log($"twoCount : {twoCount}");
+
+
+
+        //-----------------------------------------------------------------------------------------------------------------------
+        // < 3. > 算每個輪條 指定值出現的次數 （二維）
+
+
+        var value = datas.Select((x) => x.Reels.Count(item => item == (int)Number.one));
+
+        foreach (var index in value)
+        {
+            Debug.Log(index);
+        }
+        //-----------------------------------------------------------------------------------------------------------------------
+        //< 3.5 > 算指定值位於 輪調上的索引值（二維）
+
+        // var index2 = datas.Select((X,Y)=>Y).Where(Y=> datas[Y].Reels.Contains(Number.one)).Where(i => datas.Reels[i] == (int)Number.one);
+
+        // foreach (var i in index2)
+        // {
+        //     Debug.Log($"指定值位於 輪調上的索引值{i}");
+        // }
+
+        //-----------------------------------------------------------------------------------------------------------------------
+        // 算指定項目在哪個輪條（哪個輪條有指定的項目）（二維）
+
+
+        var Reel = datas.Select((X, i) => i).Where(i => datas[i].Reels.Contains(Number.one));
+
+        foreach (var i in Reel)
+        {
+            Debug.Log(i);
+        };
+
+
+
+        //-----------------------------------------------------------------------------------------------------------------------
+        //配合 上面 找出 指定圖在輪條哪個位置（二維）（指定值位於 輪調上的索引值）
+
+
+        foreach (var reel in Reel)
+        {
+            var index = datas[reel].Reels.Select((a, i) => i).Where(i => datas[reel].Reels[i] == (int)Number.one);
+            foreach (var item in index)
+            {
+                Debug.Log($"{reel},{item}");
+            }
+        }
+
+    }
+    //-----------------------------------------------------------------------------------------------------------------------
+}
+public enum Number
+{
+    one,
+    two,
+    three
+}
+
+[Serializable]
+public class Data
+{
+    public List<Number> Reels;
+}
+
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Linq說明
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ii = 0;
+            int[] iis = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] iis2 = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            int[] iis3 = new int[] { 8, 10, 9, 7, 2, 3, 5, 1, 4, 6 };
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("XDDD ");
+
+            // foreach (int i in iis)
+            // {
+            //     MyVoid(action: Prints, i);
+            // }
+
+            IEnumerable<int> values =
+                from num in iis
+                where num >= 5
+                select num;
+
+            Action<int> action = new Action<int>(Print2);
+            Array.ForEach(iis, Print2);
+
+
+            IEnumerable<int> values1 =
+                from num in iis2
+                where num >= 5
+                orderby num
+                select num;
+
+            IEnumerable<int> values2 =
+                from num in iis3
+                where num >= 5
+                select num;
+
+            IEnumerable<int> ints = iis3.Where(Num => Num > 6);
+            ;
+            Console.WriteLine($"ints.Sum(): {ints.Sum()}");
+
+            foreach (var i in values)
+            {
+                Print2(i);
+            }
+
+            Console.WriteLine("iis2-----");
+
+            foreach (var i in values1)
+            {
+
+                Print2(i);
+            }
+            Console.WriteLine("iis3-----");
+            foreach (var i in values2)
+            {
+
+                Print2(i);
+            }
+
+            MyVoid(action: Prints, 2222);
+
+        }
+
+        static void MyVoid(Action<string> action, int i)
+        {
+            action(i.ToString());
+        }
+        static void Prints(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static void Print2(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
